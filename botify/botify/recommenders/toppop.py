be9779b2ac9d540763aeaeec8821ba78e82e37fc
Nikolay Anokhin <n.anokhin@corp.mail.ru>
2022-02-17 11:19:33 +0300

    Improve interface for TopPop recommender

--- botify/botify/recommenders/toppop.py
+++ botify/botify/recommenders/toppop.py
@@ -1,10 +1,13 @@
+from typing import List
+
 from .random import Random
 from .recommender import Recommender
 
 
 class TopPop(Recommender):
-    def __init__(self, tracks_redis):
+    def __init__(self, tracks_redis, top_tracks: List[int]):
         self.random = Random(tracks_redis)
+        self.top_tracks = top_tracks
 
     # TODO 2: Implement TopPop recommender
     def recommend_next(self, user: int, prev_track: int, prev_track_time: float) -> int:
